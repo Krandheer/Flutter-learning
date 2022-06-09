b7a3523f7466165a9e063627c24261bcb431a3c7
randheer <randheer.dakshana15@gmail.com>
2022-06-09 19:10:17 +0530

    done with container widget

--- play1/lib/screen/containerWidget.dart
+++ play1/lib/screen/containerWidget.dart
@@ -7,11 +7,31 @@ class ContainerWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Contaienr widget"),
+        title: const Text("Contaienr widget"),
         centerTitle: true,
         backgroundColor: Colors.red,
       ),
-      body: Container(),
-    );
+      body: GestureDetector(
+        onTap: (){
+          print("pressed");
+        },
+        child: Center(
+          child: Container(
+              // margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(100),
+              // width: 200,
+              // height: 50,
+              // constraints: const BoxConstraints(minWidth: 10, maxWidth: 200, minHeight: 20, maxHeight: 50),
+              decoration: BoxDecoration(
+                  color: Colors.green,
+                  // borderRadius: BorderRadius.circular(20),
+                  // shape: BoxShape.circle,
+                  border: Border.all(color: Colors.red, width: 5)),
+              // alignment: Alignment.center,
+              child: const Text("body content"),
+            ),
+        ),
+      ),
+      );
   }
 }
